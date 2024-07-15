0640f4aedab967a613493a522429791312b3002e
rajyavardhanbithale <bithale03@gmail.com>
2024-07-16 00:32:06 +0530

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.resolve(__dirname, './'),
+        };
+
+        return config;
+    },
 };
 
 export default nextConfig;
