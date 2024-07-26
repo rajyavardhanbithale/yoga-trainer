8821e4ec2550a6f2c4581d139dea29140ac6be31
rajyavardhanbithale <bithale03@gmail.com>
2024-07-26 17:07:53 +0530

    remove post auth time check

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -36,10 +36,9 @@ export async function GET(request: Request) {
             // using false wale because it check if the user created time is
             // greater than Threshold time (in seconds)
             // false if the user is created in the last 6 minutes
-            if (!(await postAuth(data.user.created_at, 300))) {
-                // await createUserForDatabase(data.user)
-                return NextResponse.redirect('https://google.com/')
-            }
+           
+            await createUserForDatabase(data.user)
+            
 
             return NextResponse.redirect(`${origin}${next}`)
         }
