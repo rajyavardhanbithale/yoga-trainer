3f20ea876f3070f1e54ecf38cdd3f9c04ff595a3
rajyavardhanbithale <bithale03@gmail.com>
2024-08-15 01:43:52 +0530

    leaderboard: final

--- app/components/LeaderBoard/Leaderboard.tsx
+++ app/components/LeaderBoard/Leaderboard.tsx
@@ -2,14 +2,14 @@ import axios from 'axios'
 import LeaderboardCarousel from './LeaderboardCarousel'
 import LeaderboardDisplay from './LeaderboardDisplay'
 
-// const ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT
+const ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT
 
 export default async function Leaderboard() {
-    // const response = await axios.get(`${ENDPOINT}/api/leaderboard`)
-    // const data = response.data
+    const response = await axios.get(`${ENDPOINT}/api/leaderboard`)
+    const data = response.data
     return (
         <>
-            {/* <div className="z-50 w-11/12 justify-center glass-card">
+            <div className="z-50 w-11/12 justify-center glass-card">
                 <div className="sm:grid xl:grid-cols-6 m-3 p-3 gap-3 w-full">
                     <div className="xl:col-span-2 w-full mx-auto h-full flex justify-center items-center">
                         {data && <LeaderboardCarousel userData={data} />}
@@ -25,9 +25,7 @@ export default async function Leaderboard() {
                         <LeaderboardDisplay data={data?.metrics} />
                     </div>
                 </div>
-            </div> */}
-
-            <span>LeaderBoard</span>
+            </div>
         </>
     )
 }
