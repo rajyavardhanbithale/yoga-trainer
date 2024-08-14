b700ebb81213691a5a530bc0834d2edc72496ada
rajyavardhanbithale <bithale03@gmail.com>
2024-08-15 01:28:26 +0530

    leaderboard api route fixed

--- app/components/LeaderBoard/Leaderboard.tsx
+++ app/components/LeaderBoard/Leaderboard.tsx
@@ -6,14 +6,14 @@ import LeaderboardDisplay from './LeaderboardDisplay'
 const ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT
 
 export default async function Leaderboard() {
-    const response = await fetch(
-        `${ENDPOINT}/api/leaderboard`,
-        {
-            next: {
-                revalidate: 60,
-            },
+    const response = await fetch(`${ENDPOINT}/api/leaderboard`, {
+        next: {
+            revalidate: 60,
+        },
+        headers:{
+            'Content-Type': 'application/json',
         }
-    )
+    })
     const data = await response.json()
 
     return (
