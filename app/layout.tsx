183a0c64d7522d12bc6a535e797a9ebf62ffe9b6
rajyavardhanbithale <bithale03@gmail.com>
2024-08-19 22:55:45 +0530

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,6 +91,9 @@ export default function RootLayout({
     return (
         <StoreProvider>
             <html lang="en" data-theme="light">
+                <head>
+                    <link rel="icon" href="favicon.ico" sizes="any" />
+                </head>
                 <body className={comfortaa.className}>
                     <SmoothScrolling>{children}</SmoothScrolling>
                 </body>
